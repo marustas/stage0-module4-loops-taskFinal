b310aaeddd52b7ed8864fb3df8ccfa74013d1b58
Stanislau Marudau <marudov.stas@gmail.com>
2022-07-06 17:36:04 +0300

    First try

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,10 +2,22 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printHalfPyramid(int cathetusLength) {
+        for (int i = 1; i < cathetusLength; i++) {
+            for (int spaces = i; spaces < cathetusLength; spaces++) {
+                System.out.print(" ");
+            }
+            for (int x = i; x >= 1; x--) {
+                System.out.print(x);
+            }
 
+            for (int j = 2; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.print("\n");
+        }
     }
 
     public static void main(String[] args) {
-        new Pyramid().printHalfPyramid(7);
+        new Pyramid().printHalfPyramid(6);
     }
 }
