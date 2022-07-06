76fabcacd29c4b902c696c42dc8a4201b600ab3b
Stanislau Marudau <marudov.stas@gmail.com>
2022-07-06 18:04:41 +0300

    More Spaces added

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -3,19 +3,17 @@ package school.mjc.stage0.loops.finalTask;
 public class Pyramid {
     public void printHalfPyramid(int cathetusLength) {
         for (int i = 1; i <= cathetusLength; i++) {
-            for (int j = 0; j <= cathetusLength - i; j++) {
+            for (int spaces = i; spaces <= cathetusLength; spaces++) {
                 System.out.print(" ");
             }
-            for (int k = i; k >= 1; k--) {
-                System.out.print(k);
+            for (int x = i; x >= 1; x--) {
+                System.out.print(x);
             }
-            for (int r = 2; r <= i; r++) {
-                System.out.print(r);
+
+            for (int j = 2; j <= i; j++) {
+                System.out.print(j);
             }
-            for (int j = cathetusLength - 1; j >= i; j--) {
-                System.out.print(" ");
-            }
-            System.out.println();
+            System.out.print("\n");
         }
     }
 
